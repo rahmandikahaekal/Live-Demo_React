8c49831310ca577322f2fc0b70e853293d56fd32
rahmandikahaekal <rahmandikahaekal31@gmail.com>
2018-11-02 20:25:30 +0700

    Create a React App and publish it to GitHub Pages

---
+++ src/components/Section/Pitch/Pitch.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+const Section = styled.section`
+
+`;
+
+const Title = styled.h1`
+
+`;
+
+const Subtitle = styled.p`
+
+`;
+
+const MainJoinButton = styled.button`
+
+`;
+
+const pitch = () => {
+    return (
+        <Section>
+            <Title></Title>
+            <Subtitle></Subtitle>
+                <MainJoinButton></MainJoinButton>
+        </Section>
+    )
+}
+
+export default pitch;
